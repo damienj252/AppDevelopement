76c460ba402970e5f9c9fe0a30a0cd17950b018a
damienj252 <damienj252@gmail.com>
2015-11-16 13:36:11 +0000

    16.11.15

---
+++ UpDates/Versions1/HowToSurviveCollege/HowToSurviveCollege/HowToSurviveCollege.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HowToSurviveCollege
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //private object NavigationService;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            addTheContent();
+            
+        }//MianPage
+
+        private void addTheContent()
+        {
+            tblFood.Text = "Easy to cook dinners:" + System.Environment.NewLine + System.Environment.NewLine + 
+                            " " +
+                           "Fillet of Beef W/Sweet & Sour Peppers" + System.Environment.NewLine +
+                           "Ingrediants:" + System.Environment.NewLine +
+                           "4 Fillet steaks (170g & 3cm  thick)" + System.Environment.NewLine +
+                           "6/7 tbsp olive oil, plus extra to drizzle" + System.Environment.NewLine +
+                           "Sea salt & milled black pepper" + System.Environment.NewLine +
+                           "1 tbsp juniper berries" + System.Environment.NewLine +
+                           "Lightly crushed handful of thyme sprigs" + System.Environment.NewLine +
+                           "3 Red peppers sliced" + System.Environment.NewLine +
+                           "3 Yellow peppers sliced" + System.Environment.NewLine +
+                           "2 tbsp white wine vinegar" + System.Environment.NewLine +
+                           " " +
+                           "How to cook it:" + System.Environment.NewLine +
+                           "Place the fillet of beef in a shallow dish with 2-3 tbsp of olive oil. Season lightly and scatter over the juniper berries & a few thyme sprigs. Leave to marinate while you cook peppers." + System.Environment.NewLine +
+                           "Heat 2 tbsp olive oil in a large pan add red & yellow peppers. Add a few thyme sprigs & cook over medium heat for 5 mins. Pour in the wine vinegar& reduce right down." + System.Environment.NewLine +
+                           "Remove from heat & keep warm." + System.Environment.NewLine +
+                           "Heat another heavy-based pan add remaining olive oil. When very hot sear fillets for 3-4 mins on each side. Rest in warm place for 5 mins." + System.Environment.NewLine +
+                           "Divide peppers among warm plates. Slice fillets thickly on the diagonal & arrange on top of the peppers. Drizzle with a little olive oil to serve" + System.Environment.NewLine +
+                           " " +
+                           "Pan fried stuffed fillet of chicken" + System.Environment.NewLine +
+                           " " + System.Environment.NewLine + System.Environment.NewLine +
+                           "Ingrediants: " + System.Environment.NewLine +
+                           "1 Whole Chicken " + System.Environment.NewLine +
+                           "250ml Chicken Stock " + System.Environment.NewLine +
+                           "8 Tiger Prawns " + System.Environment.NewLine +
+                           "100g Sun dried tomatoes " + System.Environment.NewLine +
+                           "2 Large Potatoes " + System.Environment.NewLine +
+                           "3 Scallions " + System.Environment.NewLine +
+                           "100g Unsalted butter" + System.Environment.NewLine +
+                           "2 Cloves Garlic " + System.Environment.NewLine +
+                           "50ml Olive oil" + System.Environment.NewLine +
+                           "Bread 3 slices " + System.Environment.NewLine +
+                           "100g Mushrooms " + System.Environment.NewLine +
+                           "50ml Brandy" + System.Environment.NewLine +
+                           "2 medium Carrots " + System.Environment.NewLine +
+                           "2 medium Courgette" + System.Environment.NewLine +
+                           "Fresh Thyme pinch " + System.Environment.NewLine +
+                           "1 Lemon " + System.Environment.NewLine +
+                           "100g Onion " + System.Environment.NewLine +
+                           "Freshly ground mill pepper " + System.Environment.NewLine +
+                           "8 Fresh Asparagus heads " + System.Environment.NewLine +
+                           " " + System.Environment.NewLine + System.Environment.NewLine +
+                           "How to cook: " + System.Environment.NewLine +
+                           "Prepare whole chicken. " + System.Environment.NewLine +
+                           "Infuse chopped chicken bones into stock. " + System.Environment.NewLine +
+                           "Combine prawn, basil and sun dried tomatoes to stuff chicken. " + System.Environment.NewLine +
+                           "Make garlic chicken puree. " + System.Environment.NewLine +
+                           "Cook vegetables in hot stock until tender. Cook potatoes in seasoned water until tender, strain, mash, add scallions, olive oil, butter &cream. " + System.Environment.NewLine +
+                           "Pan fry chicken on both sides, finish cooking in oven. " + System.Environment.NewLine +
+                           "Spread garlic chicken puree on bread, place in hot oven until crispy. " + System.Environment.NewLine +
+                           "Sweat sliced mushrooms flame brandy, add chicken stock reduce add cream and reduce until coating consistency. " + System.Environment.NewLine +
+                           " " + System.Environment.NewLine + System.Environment.NewLine +
+                           "Pan fried salmon on a cod brandade potato " + System.Environment.NewLine +
+                           " " + System.Environment.NewLine + System.Environment.NewLine +
+                           "Ingrediants: " + System.Environment.NewLine +
+                           "2 Supremes of salmon " + System.Environment.NewLine + 
+                           "2 Anchovies " + System.Environment.NewLine +
+                           "100ml Tomato oil " + System.Environment.NewLine +
+                           "250g Salted Cod " + System.Environment.NewLine +
+                           "3 cloves of garlic " + System.Environment.NewLine +
+                           "½ each of yellow & red pepper " + System.Environment.NewLine +
+                           "3 Spring onions " + System.Environment.NewLine +
+                           "2 Cloves " + System.Environment.NewLine +
+                           "4 - 5 New potatoes " + System.Environment.NewLine +
+                           "1 Egg yolk " + System.Environment.NewLine +
+                           "Freshly Milled Pepper to season " + System.Environment.NewLine +
+                           "½ each of  courgette & red onion " + System.Environment.NewLine +
+                           "1 tsp Balsamic vinegar " + System.Environment.NewLine +
+                           "2 tsp Olive oil " + System.Environment.NewLine +
+                           "1 tsp Dijon mustard " + System.Environment.NewLine +
+                           "500g Vine Cherry tomatoes " + System.Environment.NewLine +
+                           "50 ml Hazelnut oil " + System.Environment.NewLine +
+                           "20 ml Cream " + System.Environment.NewLine +
+                           "10g Butter " + System.Environment.NewLine +
+                           "10 Basel leaves (fresh) " + System.Environment.NewLine +
+                           "5g Parsley(fresh) " + System.Environment.NewLine +
+                           " " + System.Environment.NewLine + System.Environment.NewLine +
+                           "How to cook it: " + System.Environment.NewLine +
+                           "Peel garlic. " + System.Environment.NewLine +
+                           "Make tapenade. " + System.Environment.NewLine +
+                           "Marinate salmon. " + System.Environment.NewLine +
+                           "Poach cod. " + System.Environment.NewLine +
+                           "Liquidise cherry tomatoes. " + System.Environment.NewLine +
+                           "Strain reduced sauce. " + System.Environment.NewLine +
+                           "Peel & cut potatoes. " + System.Environment.NewLine +
+                           "Prepare vegetables, herbs & spring onion, remove cooked cod. " + System.Environment.NewLine +
+                           "Cook potatoes & skim sauce. " + System.Environment.NewLine +
+                           "Cook salmon, roast vegetables. " + System.Environment.NewLine +
+                           "Prepare spring onions for garnish. " + System.Environment.NewLine +
+                           "Check brandade &sauce for seasoning. " + System.Environment.NewLine +
+                           " " + System.Environment.NewLine + System.Environment.NewLine +
+                           "Seafood chowder " + System.Environment.NewLine +
+                           " " + System.Environment.NewLine + System.Environment.NewLine +
+                           "Ingrediants: " + System.Environment.NewLine +
+                           "50g Unsalted butter " + System.Environment.NewLine +
+                           "2 Diced onions (medium) " + System.Environment.NewLine +
+                           "50g Leeks " + System.Environment.NewLine +
+                           "50g Celery(diced) " + System.Environment.NewLine +
+                           "50g Corn(fresh) " + System.Environment.NewLine +
+                           "250g Potatoes(diced) " + System.Environment.NewLine +
+                           "120g Smoked Fish " +
+                           "200g Fresh Fish (prawns, shrimp, mussels, Salmon, monkfish) " + System.Environment.NewLine +
+                           "1 Bay leaf " + System.Environment.NewLine +
+                           "80ml White Wine " + System.Environment.NewLine +
+                           "600ml Fish stock " + System.Environment.NewLine +
+                           "150ml Cream " + System.Environment.NewLine +
+                           "20g Fresh Parsley(chopped) " + System.Environment.NewLine +
+                           "30g Fresh Dill(chopped) " + System.Environment.NewLine +
+                           "20g Fresh Tarragon(chopped) " + System.Environment.NewLine +
+                           "Freshly Milled Pepper to season " + System.Environment.NewLine +
+                           " " + System.Environment.NewLine + System.Environment.NewLine +
+                           "How to cook: " + System.Environment.NewLine +
+                           "Melt butter in large pot, add leeks, celery and onions, cook for 3 - 4 minutes without colour(sweat). " + System.Environment.NewLine +
+                           "Add potatoes, bay leaf and white wine cook for a further 3 minutes. " + System.Environment.NewLine +
+                           "Add fish stock and bring to the boil. " + System.Environment.NewLine +
+                           "Add the fish and simmer for 5 - 8 minutes over a medium heat. " + System.Environment.NewLine +
+                           "Add cream and fresh herbs(parsley, dill, tarragon) season with freshly milled pepper, bring back to the boil. " + System.Environment.NewLine +
+                           "Serve with wholemeal brown bread. ";
+
+           tblAccomodation.Text = "Tips on getting your deposit back:" + System.Environment.NewLine +
+                                    "Record all bill payments, by law they are recorded in your rent or by receipt." + System.Environment.NewLine +
+                                    "All appliances and furniture must be listed." + System.Environment.NewLine +
+                                    "Take photographs of every room and all damaged items if any when you move in and out." + System.Environment.NewLine +
+                                    "Look after your accomodation and inform your landlord of any damages." + System.Environment.NewLine +
+                                    "Give appropriate notice when leaving." + System.Environment.NewLine +
+                                    "Make sure all bills are paid for before leaving." + System.Environment.NewLine +
+                                    "When you leave your landlord must return your deposit unless it is to pay for damages or if you have signed a contract saying that they can give it back on a later date." + System.Environment.NewLine +
+                                    "The deposit belongs to you NOT your landlord." + System.Environment.NewLine +
+                                    "Ask for a receipt of damages in case of unfair deductions and query about them.";
+
+            tblNightLife.Text = "Welcome to Galway, get ready for the time of your life "  + System.Environment.NewLine +
+                                "There are many great places to go out in Galway." + System.Environment.NewLine +
+                                "Monday nights I would recomend Carbon the night club." + System.Environment.NewLine +
+                                "Tuesday drinks in. " + System.Environment.NewLine +
+                                "Wedbesday: depending on promotions I would recommend Electric" + System.Environment.NewLine +
+                                "Thursday: I wouuld recommend 44 " + System.Environment.NewLine +
+                                "Great student pubs include Fibber MeGees, Hole in the wall and the front door." + System.Environment.NewLine +
+                                "There is such a variety of places to go out in Galway it's hard to say which is the best but my advice is try them all. " + System.Environment.NewLine +
+                                "Don't forget, never walk home alone " + System.Environment.NewLine +
+                                "Welcome to Galway, get ready for the time of your life ";
+
+            tblCollegeServices.Text = "There are many great services in all colleges in Galway." + System.Environment.NewLine +
+                                       "College councilers " + System.Environment.NewLine +
+                                       "College bars. " + System.Environment.NewLine +
+                                       "Shops: food and stationary for college " + System.Environment.NewLine +
+                                       "Students Union " + System.Environment.NewLine +
+                                       "Library with every book you could ever need as well as printing etc. " + System.Environment.NewLine +
+                                       "Internet connection. " + System.Environment.NewLine +
+                                       "Free grinds and IT center. " ;
+
+
+
+        }//addTheContent()
+
+
+
+    }//partial class
+}//namespace
---
+++ UpDates/Versions3/HowToSurviveCollege/HowToSurviveCollege/HowToSurviveCollege.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HowToSurviveCollege
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            addTheContent();
+            
+        }//MianPage
+
+        private void addTheContent()
+        {
+            tblStart.Text = @"This app is for any student living in Galway
+Galway is an amzing town to live in
+There are so many different cultures here and each and everyday is an expericence on its own
+Go out there and try everything
+They have variety of foods, bars and entertainment
+It has everything anyone of any age coould wish for as it is a city for all ages
+Some things to do in Galway";
+
+
+
+
+                tblFood.Text = @"Fillet of Beef W/Sweet & Sour Peppers
+Ingredients:
+
+4 Fillet steaks (170g & 3cm thick)
+6/7 tbsp. olive oil, plus extra to drizzle
+Sea salt & milled black pepper
+1 tbsp. juniper berries
+Lightly crushed handful of thyme sprigs
+3 Red peppers sliced
+3 Yellow peppers sliced
+2 tbsp. white wine vinegar
+
+How to cook it:
+
+Place the fillet of beef in a shallow dish with 2-3 tbsp. of olive oil. Season lightly and scatter over the juniper berries & a few thyme sprigs. Leave to marinate while you cook peppers
+Heat 2 tbsp. olive oil in a large pan add red & yellow peppers. Add a few thyme sprigs & cook over medium heat for 5 mins. Pour in the wine vinegar& reduce right down
+Remove from heat & keep warm
+Heat another heavy-based pan add remaining olive oil. When very hot sear fillets for 3-4 mins on each side. Rest in warm place for 5 mins
+Divide peppers among warm plates. Slice fillets thickly on the diagonal & arrange on top of the peppers. Drizzle with a little olive oil to serve
+
+Pan fried stuffed fillet of chicken
+
+Ingredients:
+
+1 Whole Chicken 
+250ml Chicken Stock 
+8 Tiger Prawns
+100g Sun dried tomatoes
+2 Large Potatoes
+3 Scallions
+100g Unsalted butte
+2 Cloves Garlic 
+50ml Olive oil
+Bread 3 slices
+100g Mushrooms
+50ml Brandy
+2 medium Carrots
+2 medium Courgette
+Fresh Thyme pinch 
+1 Lemon
+100g Onion
+Freshly ground mill pepper 
+8 Fresh Asparagus heads 
+                          
+How to cook it:
+
+Prepare whole chicken
+Infuse chopped chicken bones into stock
+Combine prawn, basil and sun dried tomatoes to stuff chicken
+Make garlic chicken puree
+Cook vegetables in hot stock until tender. Cook potatoes in seasoned water until tender, strain, mash, add scallions, olive oil, butter &cream
+Pan Fry chicken on both sides, finish cooking in oven
+Spread garlic chicken puree on bread, place in hot oven until crispy
+Sweat sliced mushrooms flame brandy, add chicken stock reduce add cream and reduce until coating consistency
+
+Pan fried salmon on a cod brandied potato
+
+Ingredients:
+
+2 Supremes of salmon 
+2 Anchovies
+100ml Tomato oil
+250g Salted Cod
+3 cloves of garlic
+½ each of yellow & red pepper
+3 Spring onions 
+2 Cloves
+4 - 5 new potatoes
+Freshly Milled Pepper to season
+½ each of courgette & red onion 
+1 tsp Balsamic vinegar
+2 tsp Olive oil 
+1 tsp Dijon mustard
+500g Vine Cherry tomatoes
+50 ml Hazelnut oil 
+20 ml Cream 
+10g Butter 
+10 Basel leaves (fresh) 
+5g Parsley (fresh)
+                          
+How to cook it:
+
+Peel garlic
+Make tapenade
+Marinate salmon
+Poach cod
+Liquidise cherry tomatoes
+Peel & cut potatoes
+Prepare vegetables, herbs & spring onion, remove cooked cod
+Cook potatoes & skim sauce
+Cook salmon, roast vegetables
+Prepare spring onions for garnish
+Check brandied &sauce for seasoning
+                          
+Seafood chowder
+                           
+Ingredients:
+
+50g Unsalted butter
+2 Diced onions (medium)
+50g Leeks
+50g Celery (diced)
+50g Corn (fresh)
+250g Potatoes (diced)
+120g Smoked Fish
+200g Fresh Fish (prawns, shrimp, mussels, Salmon, monkfish)
+1 Bay leaf
+80ml White Wine
+600ml Fish stock
+150ml Cream
+20g Fresh Parsley (chopped)
+30g Fresh Dill (chopped)
+20g Fresh Tarragon (chopped)
+Freshly Milled Pepper to season
+                          
+
+How to cook it:
+
+Melt butter in large pot, add leeks, celery and onions, and cook for 3 - 4 minutes without colour (sweat)
+Add potatoes, bay leaf and white wine cook for a further 3 minutes
+Add fish stock and bring to the boil
+Add the fish and simmer for 5 - 8 minutes over a medium heat
+Add cream and fresh herbs (parsley, dill, tarragon) season with freshly milled pepper, bring back to the boil
+Serve with wholemeal brown bread
+
+Spaghetti 
+
+Ingredients:
+
+a box of spaghetti
+a jar of marinara sauce
+1-2 pounds of ground beef or Italian sausage
+two large pots, one for boiling the pasta and the other for making the sauce
+serving untensils
+salt
+water
+
+How to cook it:
+
+Before you start, fill up a large pot with water and add a handful of salt. Put it on the stove and turn it to high so it'll start boiling!
+Next you will need to cook the of meat. Take all of the meat out of its container and set it all in a large pan, then put the pan on the stove and set the stove to medium
+After that you will need to break the meat into smaller pieces
+When the meat is in small pieces you will then need to start flipping the meat and make sure it gets brown all-around and cooked thoroughly 
+The cooking should take approximately 10-15 minutes 
+When all of the meat is brown and cooked you can add in the sauce
+Next you will need to mix your marinara sauce with your cooked meat
+Take your jar of marinara sauce and pour all of it on the cooked meat and then stir it all around until it is all blended together
+Then turn the heat down to low and let it simmer while you cook the pasta
+Once the water has come up to a boil, you can throw the spaghetti in. 
+Typically it should cook within 7-11 minutes - just follow the time on the box!
+Make sure to stir every couple minutes to keep it from sticking.
+Depending on your timing, you can either strain the spaghetti and set it aside, or you can put it right into the sauce
+
+Broccoli Chicken Casserole
+
+2 cups sour cream 1/4
+cup grated Parmesan cheese
+1 cup shredded Cheddar cheese 
+Salt and pepper to taste
+1 clove garlic, crushed 
+2 tablespoons butter
+Butter Light 1 (8 ounce)
+1 pound package wide egg noodles
+Market Pantry Frozen Chicken Breasts 
+ 1 (10.75 ounce) can condensed cream of chicken soup 
+1 cup sliced black olives 
+2 heads broccoli, cut into florets 
+1 teaspoon curry powder
+
+How to cook it:
+
+Preheat oven to 325 degrees F (165 degrees C)
+In a medium bowl, combine the sour cream, Parmesan cheese, Cheddar cheese, salt, pepper and garlic and mix together
+In a large bowl, stir butter into noodles, then stir in sour cream mixture
+In a third large bowl combine the chicken, soup, olives, and broccoli and mix together, add this mixture to the noodle mixture and mix all together
+Spread mixture into a 9x13 inch baking dish and sprinkle with curry powder
+Bake at 325 degrees F (165 degrees C) for 30 minutes
+
+All These products can be purchased in Aldi/Lidl for less than €17 if you shop right by purchasing the homebrand products";
+
+
+            tblAccomodation.Text = @"Tips on getting your deposit back:
+
+Record all bill payments, by law they are recorded in your rent or by receipt
+All appliances and furniture must be listed
+Take photographs of every room and all damaged items if any when you move in and out
+Look after your accommodation and inform your landlord of any damages
+Give appropriate notice when leaving
+Make sure all bills are paid for before leaving
+When you leave your landlord must return your deposit unless it is to pay for damages or if you have signed a contract saying that they can give it back on a later date
+The deposit belongs to you NOT your landlord
+Ask for a receipt of damages in case of unfair deductions and query about them.";
+
+
+            tblNightLife.Text = @"Welcome to Galway, get ready for the time of your life:
+
+There are many great places to go out in Galway:
+Monday nights I would recommend Carbon the night club
+Tuesday drinks in
+Wednesday: depending on promotions I would recommend Electric
+Thursday: I would recommend 44
+Great student pubs include Fibber Magees, McGettigan’s, Hole in the wall and the front door
+There is such a variety of places to go out in Galway it's hard to say which is the best but my advice is try them all
+Don't forget, never walk home alone 
+Welcome to Galway, get ready for the time of your life";
+
+
+            tblCollegeServices.Text = @"There are many great services in all colleges in Galway:
+
+College bars
+Shops: food and stationary for college
+Students Union
+Library with every book you could ever need as well as printing etc.
+Internet connection
+Free grinds and IT centre";
+
+        }//addTheContent()
+
+    }//partial class
+}//namespace
